483144542ef27cfb7d362d6afbe2b2ffc2bed34f
Justin Schneck <jschneck@mac.com>
2021-12-01 21:05:56 -0500

    Add cli module and parse commands for Node

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+mod cli;
+mod node;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opts = cli::Opt::from_args();
+    opts.handle_command().await;
+    Ok(())
 }
