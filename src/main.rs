7db78a7b174fbe10ad426158aeb5cc1caf3f91fe
abelino <abelino.romo@gmail.com>
2022-03-16 15:04:05 -0700

    PRO-205: Integrate peridio-sdk api client and improve module layout (#5)
    - Integrate peridio_sdk::Api client.
    - Add new `identity` cli command.
    - Compartmentalize `api` and `agent` commands.
    - Simplify main module via separation of concerns.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
-mod cli;
-mod node;
+mod agent;
+mod api;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Program {
+    #[structopt(subcommand)]
+    command: Command,
+}
+
+impl Program {
+    async fn run(self) -> Result<(), Box<dyn std::error::Error>> {
+        match self.command {
+            Command::Api(cmd) => cmd.run().await?,
+            Command::Node(cmd) => cmd.run().await?,
+        };
+
+        Ok(())
+    }
+}
+
+#[derive(StructOpt)]
+#[structopt(about = "interact with local or network connected nodes")]
+enum Command {
+    #[structopt(flatten)]
+    Api(api::ApiCommand),
+
+    /// Interact with local or network connected nodes
+    Node(agent::AgentCommand),
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let opts = cli::Opt::from_args();
-    opts.handle_command().await;
-    Ok(())
+    Program::from_args().run().await
 }
