e368988f9bd5df51c2a31543a88c5b78697631b2
andresatvery <andres.h@verypossible.com>
2022-09-29 21:42:00 -0500

    ENG-487: add CLI commands
    - Organization users.
    - Product users.

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2,6 +2,8 @@ mod deployments;
 mod device_certificates;
 mod devices;
 mod firmwares;
+mod organization_users;
+mod product_users;
 mod products;
 mod signing_keys;
 mod upgrade;
@@ -27,7 +29,9 @@ pub enum ApiCommand {
     Devices(devices::DevicesCommand),
     DeviceCertificates(device_certificates::DeviceCertificatesCommand),
     Firmwares(firmwares::FirmwaresCommand),
+    OrganizationUsers(organization_users::OrganizationUsersCommand),
     Products(products::ProductsCommand),
+    ProductUsers(product_users::ProductUsersCommand),
     SigningKeys(signing_keys::SigningKeysCommand),
     #[structopt(flatten)]
     Upgrade(upgrade::UpgradeCommand),
@@ -41,7 +45,9 @@ impl ApiCommand {
             ApiCommand::Devices(cmd) => cmd.run().await?,
             ApiCommand::DeviceCertificates(cmd) => cmd.run().await?,
             ApiCommand::Firmwares(cmd) => cmd.run().await?,
+            ApiCommand::OrganizationUsers(cmd) => cmd.run().await?,
             ApiCommand::Products(cmd) => cmd.run().await?,
+            ApiCommand::ProductUsers(cmd) => cmd.run().await?,
             ApiCommand::SigningKeys(cmd) => cmd.run().await?,
             ApiCommand::Users(cmd) => cmd.run().await?,
             ApiCommand::Upgrade(cmd) => cmd.run().await?,
